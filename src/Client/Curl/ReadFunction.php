f62139160547a8636c332db1be4c74757c8b583e
Evgeny Soynov <evgeny.soynov@sio.ag>
2016-04-14 11:30:56 +0200

    Improves testability
    * use dev-master branch of coverage library which allows php 5.4
    * fix ReadFunction, so that should no longer fail with php 7
    * remove SpeechFactory as it should be no longer used

--- src/Client/Curl/ReadFunction.php
+++ src/Client/Curl/ReadFunction.php
@@ -21,12 +21,12 @@ class ReadFunction
     }
 
     /**
-     * @param int $_ not used
-     * @param int $_ not used
+     * @param int $unused1 not used
+     * @param int $unused2 not used
      * @param int $length amount of data to send to server
      * @return string
      */
-    public function read($_, $_, $length)
+    public function read($unused1, $unused2, $length)
     {
         return $this->stream->read($length);
     }
